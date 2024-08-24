e8b7bea2a01b49b766b6d4d0d3e851e2825076ad
Doeun <ide05484@gmail.com>
2024-08-24 14:40:22 +0900

    :lipstick: Design: App.css 앱 화면 적용

--- so-go-client/src/pages/common/Layout.jsx
+++ so-go-client/src/pages/common/Layout.jsx
@@ -1,15 +1,12 @@
-import '@/css/common/Layout.css';
-import React from 'react';
-import Header from '@/pages/common/TheHeader';
-import Menu from '@/pages/common/Menu';
+import React from "react";
+import Header from "@/pages/common/TheHeader";
+import Menu from "@/pages/common/Menu";
 
 export default function Layout(props) {
   return (
     <div>
       <Header />
-      <main className='layout'>
-        {props.children}
-      </main>
+      <main id="layout">{props.children}</main>
       <Menu />
     </div>
   );

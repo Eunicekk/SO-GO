dc18ea8b6bc5a42d78c03b1a7fab68afb34e8d40
Eunicekk <cho_lyn@naver.com>
2024-09-14 17:14:47 +0900

    🐛 [Fix] Fix Login

--- so-go-client/src/App.jsx
+++ so-go-client/src/App.jsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "@/pages/common/Layout";
 import MainPage from "@/pages/main/MainPage";
-import TabMenu from "./pages/main/TabMenu";
+import TabMenu from "@/pages/main/TabMenu";
 
+import Loading from "@/pages/login/Loading";
 import Login from "@/pages/login/Login";
 
 import ReviewWrite from "@/pages/review/ReviewWrite";
-import ReviewDetail from "./pages/review/ReviewDetail";
+import ReviewDetail from "@/pages/review/ReviewDetail";
 
 import Map from "@/pages/map/Map";
 
@@ -19,74 +20,84 @@ import SearchResultList from "./pages/search/SearchResultList";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className={`route-wrapper`}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <MainPage />
-              </Layout>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/menu" element={<TabMenu />} />
+	return (
+		<div className="App">
+			<div className={`route-wrapper`}>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<Layout>
+								<MainPage />
+							</Layout>
+						}
+					/>
+					<Route
+						path="/loading"
+						element={<Loading />}
+					/>
+					<Route
+						path="/login"
+						element={<Login />}
+					/>
+					<Route
+						path="/menu"
+						element={<TabMenu />}
+					/>
 
-          <Route
-            path="/create"
-            element={
-              <Layout>
-                <ReviewWrite />
-              </Layout>
-            }
-          />
-          <Route
-            path="/review"
-            element={
-              <Layout>
-                <ReviewDetail />
-              </Layout>
-            }
-          />
+					<Route
+						path="/create"
+						element={
+							<Layout>
+								<ReviewWrite />
+							</Layout>
+						}
+					/>
+					<Route
+						path="/review"
+						element={
+							<Layout>
+								<ReviewDetail />
+							</Layout>
+						}
+					/>
 
-          <Route
-            path="/map"
-            element={
-              <Layout>
-                <Map />
-              </Layout>
-            }
-          />
-          <Route
-            path="/my-page"
-            element={
-              <Layout>
-                <MyPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/place"
-            element={
-              <Layout>
-                <PlaceDetail />
-              </Layout>
-            }
-          />
-          <Route
-            path="/search"
-            element={
-              <Layout>
-                <SearchResultList />
-              </Layout>
-            }
-          />
-        </Routes>
-      </div>
-    </div>
-  );
+					<Route
+						path="/map"
+						element={
+							<Layout>
+								<Map />
+							</Layout>
+						}
+					/>
+					<Route
+						path="/my-page"
+						element={
+							<Layout>
+								<MyPage />
+							</Layout>
+						}
+					/>
+					<Route
+						path="/place"
+						element={
+							<Layout>
+								<PlaceDetail />
+							</Layout>
+						}
+					/>
+					<Route
+						path="/search"
+						element={
+							<Layout>
+								<SearchResultList />
+							</Layout>
+						}
+					/>
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- so-go-client/src/axios/get-decoding.js
+++ so-go-client/src/axios/get-decoding.js
@@ -1,17 +1,26 @@
+// Base64Url 디코딩 함수
+const decodeBase64Url = (base64Url) => {
+	const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+	const padding = base64.length % 4 === 0 ? "" : "=".repeat(4 - (base64.length % 4));
+	const base64WithPadding = base64 + padding;
+	const decoded = atob(base64WithPadding);
+	return decoded;
+};
+
 const getTokenInfo = (token) => {
-  try {
-    const parts = token.split(" ");
-    if (parts.length === 2 && parts[0] === "Bearer") {
-      const jwtToken = parts[1];
-      const payloadBase64Url = jwtToken.split(".")[1];
-      const decodedPayload = decodeBase64Url(payloadBase64Url);
-      const decodedToken = JSON.parse(decodedPayload);
-      return { userUuid: decodedToken.userUuid, role: decodedToken.role };
-    }
-  } catch (error) {
-    console.error("Error decoding token:", error);
-    return { userTsid: null, userRole: null };
-  }
-}
+	try {
+		const parts = token.split(" ");
+		if (parts.length === 2 && parts[0] === "Bearer") {
+			const jwtToken = parts[1];
+			const payloadBase64Url = jwtToken.split(".")[1];
+			const decodedPayload = decodeBase64Url(payloadBase64Url);
+			const decodedToken = JSON.parse(decodedPayload);
+			return { userUuid: decodedToken.userUuid, role: decodedToken.role };
+		}
+	} catch (error) {
+		console.error("Error decoding token:", error);
+		return { userTsid: null, userRole: null };
+	}
+};
 
 export { getTokenInfo };

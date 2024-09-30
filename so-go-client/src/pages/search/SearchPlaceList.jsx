3d1c51a255a828fd37611eb35e743bb9bed8db5e
Eunicekk <cho_lyn@naver.com>
2024-09-30 23:59:45 +0900

    ✨ [Feat] 장소 검색 목록 연결

--- so-go-client/src/pages/search/SearchPlaceList.jsx
+++ so-go-client/src/pages/search/SearchPlaceList.jsx
@@ -1,23 +1,7 @@
 import "@/css/search/SearchPlaceList.css";
 import SearchPlaceListItem from "@/components/searchList/SearchPlaceListItem";
-import { useEffect } from "react";
 
 export default function SearchPlaceList({ result }) {
-	cosnt[(address, setAddress)] = useState([]);
-
-	useEffect(() => {
-		const { kakao } = window;
-		const geocoder = new kakao.maps.services.Geocoder();
-
-		const fetchAddress = async () => {
-			const addressPRomise = result.map((place) => {
-				const coords = new kakao.maps.LatLng(place.lat, place.lng);
-
-				return new Promise((resolve, reject) => {});
-			});
-		};
-	});
-
 	return (
 		<div id="place-list">
 			<div id="place-title">
@@ -26,15 +10,19 @@ export default function SearchPlaceList({ result }) {
 			</div>
 
 			<div className="place-content">
-				{result.from({ length: 5 }).map((place, index) => (
-					<SearchPlaceListItem
-						key={index}
-						thumbnail={place.placeImgs}
-						name={place.placeName}
-						address={place.address}
-						detail={place.tag}
-					/>
-				))}
+				{result.length > 0 ? (
+					result.slice(0, 5).map((place) => (
+						<SearchPlaceListItem
+							key={place.placeUuid}
+							thumbnail={place.placeImgs}
+							name={place.placeName}
+							address={place.address}
+							tag={place.tag}
+						/>
+					))
+				) : (
+					<p>관련 장소가 없습니다.</p>
+				)}
 			</div>
 		</div>
 	);

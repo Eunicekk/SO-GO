03da5b66cbc338c33c162b60df256574b82ed257
Eunicekk <cho_lyn@naver.com>
2024-10-02 02:08:27 +0900

    🎨 [Style] 리뷰 상세 CSS 개선

--- so-go-client/src/pages/review/CommentList.jsx
+++ so-go-client/src/pages/review/CommentList.jsx
@@ -24,20 +24,24 @@ function CommentList({ commentList, reviewUuid }) {
 							className={`comment-container ${index > 0 ? "reply-comment" : ""}`} // 대댓글일 때 클래스 추가
 						>
 							<div className="commenter-profile">
-								<div className="profileimg-name-info">
-									<img
-										src={comment.userImg || DefaultProfile} // 이미지가 없을 때 기본 이미지
-										alt="댓글프로필사진"
-										className="info-profile-img"
-									/>
+								<div className="profile-info">
+									<div className="profile-img">
+										<img
+											src={comment.userImg || DefaultProfile} // 이미지가 없을 때 기본 이미지
+											alt="댓글프로필사진"
+											className="info-profile-img"
+										/>
+									</div>
 									<span>{comment.userNickname}</span>
 								</div>
+
 								<DotsThreeVertical
 									ref={dotsRef}
 									size={24}
 									onClick={onOpenReport}
 									style={{ cursor: "pointer" }}
 								/>
+
 								{showReport && (
 									<div className="dropdown">
 										<ReportComment

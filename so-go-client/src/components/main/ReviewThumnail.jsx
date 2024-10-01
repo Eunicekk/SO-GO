5b238acb41e0d662a8c6a76e7a09ca213f184ba0
Eunicekk <cho_lyn@naver.com>
2024-10-01 23:43:20 +0900

    💄 [Style] 메인 CSS 개선

--- so-go-client/src/components/main/ReviewThumnail.jsx
+++ so-go-client/src/components/main/ReviewThumnail.jsx
@@ -3,24 +3,24 @@ import DefaultProfile from "@/assets/profile.png";
 
 function ReviewThumnail({ review, onClick }) {
 	return (
-		<div>
-			<div
-				className="review-thumbnail"
-				onClick={onClick}
-			>
-				<div className="thumbnail">
-					<img
-						src={review.img}
-						alt="맛집 이미지"
-					/>
-				</div>
-				<div className="content">
+		<div
+			className="review-items"
+			onClick={onClick}
+		>
+			<div className="thumbnail">
+				<img
+					src={review.img}
+					alt="맛집 이미지"
+				/>
+			</div>
+			<div className="content">
+				<div className="img-wrap">
 					<img
 						src={review.userImg || DefaultProfile}
 						alt="유저프사"
 					/>
-					<span>{review.userNickname}</span>
 				</div>
+				<span>{review.userNickname}</span>
 			</div>
 		</div>
 	);
